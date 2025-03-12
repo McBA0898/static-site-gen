d85da9d9c30fe133f110e07edc1014fc66b5be09
McBA0898 <bmccaleb803@gmail.com>
2025-03-12 14:50:42 -0500

    add extract_markdown functions and tests

--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_parser import split_nodes_delimiter
+from markdown_parser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -39,6 +39,18 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is plain text", TextType.NORMAL_TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
         self.assertEqual(new_nodes, [node])
+
+    def text_extract_markdown_images(self):
+        matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def text_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
